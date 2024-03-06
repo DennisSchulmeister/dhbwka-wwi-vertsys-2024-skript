9499ae5510c13ec406e47e5e3be124c3a3c3093f
Dennis Schulmeister-Zimolong <dennis@patk.org>
2024-03-06 03:12:53 +0100

    Kleiner Fix: Docker -> Service Discovery

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,5 @@ set -e
 
 npm run build
 #rsync -rvz build/ dennis@pingu-mobil.de:/srv/www/www.wpvs.de/repo/vertsys-2024/skript
-#rsync -rvz build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/vertsys-2024/skript
-rsync -rvz -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/vertsys-2024/skript
+rsync -rvz build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/vertsys-2024/skript
+#rsync -rvz -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/vertsys-2024/skript
