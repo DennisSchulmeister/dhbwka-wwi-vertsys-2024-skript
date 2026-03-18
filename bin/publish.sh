a7972027bd53052a52c7a1f82ea9b752169c200f
Dennis Schulmeister-Zimolong <dennis@patk.org>
2026-03-18 17:22:43 +0100

    rsync: Alte Dateien löschen, Timestamps übernehmen → Browser Caching

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,6 +2,6 @@
 set -e
 
 npm run build
-#rsync -rvz build/ dennis@pingu-mobil.de:/srv/www/www.wpvs.de/repo/vertsys/skript
-rsync -rvz build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/vertsys/skript
-#rsync -rvz -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/vertsys/skript
+#rsync -avz --delete --checksum build/ dennis@pingu-mobil.de:/srv/www/www.wpvs.de/repo/vertsys/skript
+rsync -avz --delete --checksum build/ dennis@vhermes:/srv/www/www.wpvs.de/repo/vertsys/skript
+#rsync -avz --delete --checksum -e 'ssh -p 4444' build/ dennis@wikiberd.de:/srv/www/www.wpvs.de/repo/vertsys/skript
